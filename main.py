985bd85c52908c66f5b234269b9787a73c18cde1
Aristana Scourtas <ascourtas@gmail.com>
2020-05-21 22:02:48 -0500

    add main.py for future implementation

---
+++ main.py
@@ -0,0 +1,3 @@
+# TODO: come up with pipeline all together before implementing here
+# this will be the primary call script for now, should run whole pipeline by calling necessary classes/methods
+# from /src
